6f4ce3ae5c622142b0c97051034e0558d1b1005f
Pvadgal <pranathi.r125@gmail.com>
2025-09-06 13:00:48 +0530

    Update project.v
    initial mux

--- src/project.v
+++ src/project.v
@@ -15,13 +15,14 @@ module tt_um_example (
     input  wire       clk,      // clock
     input  wire       rst_n     // reset_n - low to reset
 );
-
-  // All output pins must be assigned. If not used, assign to 0.
-  assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
-  assign uio_out = 0;
-  assign uio_oe  = 0;
-
+    assign ui_in[0] = a;
+    assign ui_in[1] = b;
+    assign ui_in[2] = sel;
+    
+assign y = sel ? b : a;
+    assign u_out[0] = y;
+    assign uio_out[7:0] = 7'b0;
   // List all unused inputs to prevent warnings
-  wire _unused = &{ena, clk, rst_n, 1'b0};
+    wire _unused = &{ena, clk, rst_n, 1'b0, uio_in[7,0]};
 
 endmodule
